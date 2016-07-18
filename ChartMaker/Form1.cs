819fa6cd83eb8def0100189acb260306d41d1560
bciven <behrad.farsi@gmail.com>
2016-07-17 23:23:33 -0400

    chart

---
+++ ChartMaker/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChartMaker
+{
+    public partial class Charts : Form
+    {
+        public Charts()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            var browser = new FolderBrowserDialog {SelectedPath = Directory.GetCurrentDirectory()};
+            var dlgResult = browser.ShowDialog();
+            if (dlgResult == DialogResult.OK)
+            {
+                textBoxFolder.Text = browser.SelectedPath;
+            }
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            if (textBoxFolder.Text == "")
+            {
+                return;
+            }
+            var welfares = ReadData.CalcAverageWelfares(textBoxFolder.Text);
+
+            chart.DataSource = welfares;
+        }
+    }
+}
---
+++ ChartMaker/ReadData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+
+namespace ChartMaker
+{
+    public static class ReadData
+    {
+        public static List<double> CalcAverageWelfares(string folder)
+        {
+            var files = Directory.GetFiles(folder);
+            var groups = files.GroupBy(x => x.Split('-')[0]);
+            var averages = new List<double>();
+            foreach (var @group in groups)
+            {
+                var welfares = @group.Select(ReadSocialWelfare).ToList();
+                averages.Add(welfares.Average(x => x));
+            }
+            return averages;
+        }
+
+        private static double ReadSocialWelfare(string file)
+        {
+            ExcelPackage package = new ExcelPackage(new FileInfo(file));
+            var ws = package.Workbook.Worksheets["Assignments"];
+            var address = ws.Cells.First(x => x.Text == "Social Welfare").Start;
+            var value = ws.Cells[address.Row, 2].Value;
+            return Convert.ToDouble(value);
+        }
+    }
+}
