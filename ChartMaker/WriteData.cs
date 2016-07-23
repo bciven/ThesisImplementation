987f160890ce3b34b9a8ca80afcb2b540753ab25
bciven <behrad.farsi@gmail.com>
2016-07-23 01:12:46 -0400

    experiments

--- ChartMaker/WriteData.cs
+++ ChartMaker/WriteData.cs
@@ -13,25 +13,51 @@ namespace ChartMaker
     {
         public static void Write(string folder, List<List<AlgorithmWelfare>> welfares)
         {
-            var file = new FileInfo(Path.Combine(folder, "chart.xlsx"));
+
+            var file = new FileInfo(Path.Combine(folder, DateTime.Now.ToFileTime() + ".xlsx"));
             var package = new ExcelPackage(file);
             var ws = package.Workbook.Worksheets.Add("Chart");
-            
+            var welfareChart = ws.Drawings.AddChart("chart1", eChartType.Line);
+            var regretChart = ws.Drawings.AddChart("chart2", eChartType.Line);
+
+            ws.Cells[1, 1].Value = "User Count";
+            ws.Cells[1, 2].Value = "Event Count";
+            ws.Cells[1, 3].Value = "Alpha";
+            var rows = welfares.Count + 1;
             for (int i = 0; i < welfares.Count; i++)
             {
-                ws.Cells[1, (2 * i + 1)].Value = welfares[i][0].UserCount;
-                ws.Cells[1, (2 * i + 2)].Value = welfares[i][0].EventCount;
-                ws.Cells[2, (2 * i + 1)].Value = welfares[i][0].Alpha;
-
-                for (int j = 0; j < welfares[i].Count; j++)
+                ws.Cells[i + 2, 1].Value = welfares[i][0].UserCount;
+                ws.Cells[i + 2, 2].Value = welfares[i][0].EventCount;
+                ws.Cells[i + 2, 3].Value = welfares[i][0].Alpha;
+                var col = 4;
+                for (int j = 0; j < welfares[i].Count; j++, col++)
                 {
-                    ws.Cells[j + 3, (2 * i + 1)].Value = welfares[i][j].Version;
-                    ws.Cells[j + 3, (2 * i + 2)].Value = welfares[i][j].AvgWelfare;
-                    ws.Cells[j + 3, (2 * i + 3)].Value = welfares[i][j].AvgRegRatio;
+                    ws.Cells[1, col].Value = welfares[i][j].Version;
+                    ws.Cells[i + 2, col].Value = welfares[i][j].AvgWelfare;
+                    if (i == 0)
+                    {
+                        welfareChart.Series.Add(ws.Cells[2, col, rows, col], ws.Cells[2, 2, rows, 2]);
+                        welfareChart.Series[welfareChart.Series.Count - 1].HeaderAddress = ws.Cells[1, col];
+                    }
                 }
-                var chart = ws.Drawings.AddChart("chart", eChartType.Line);
-                chart.Series.Add(ws.Cells[3, (2*i + 1), 3 + welfares[i].Count, (2*i + 2)], ws.Cells[1, (2*i + 2)]);
+
+                for (int j = 0; j < welfares[i].Count; j++, col++)
+                {
+                    ws.Cells[1, col].Value = welfares[i][j].Version;
+                    ws.Cells[i + 2, col].Value = welfares[i][j].AvgWelfare;
+                    if (i == 0)
+                    {
+                        regretChart.Series.Add(ws.Cells[2, col, rows, col], ws.Cells[2, 2, rows, 2]);
+                        regretChart.Series[regretChart.Series.Count - 1].HeaderAddress = ws.Cells[1, col];
+                    }
+                }
+
             }
+            welfareChart.SetPosition(1, 0, ws.Dimension.End.Column + 1, 0);
+            //welfareChart.SetSize(400,400);
+            regretChart.SetPosition(ws.Dimension.End.Row + 1, 0, ws.Dimension.End.Column + 1, 0);
+            //regretChart.SetSize(400,400);
+
             package.Save();
         }
     }
